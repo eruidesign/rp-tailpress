78a56c9f853c2d74f54bc74ff1cdc8d81762debd
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-08-25 13:24:02 +0800

    Query only parents

--- functions.php
+++ functions.php
@@ -242,3 +242,21 @@ function codemode__show_linked_product( $content ){
 
     return $content.$data; 
 }
+
+
+function exclude_product_cat_children($wp_query) {
+    if ( is_product_category() ) {
+        if ( isset ( $wp_query->query_vars['product_cat'] ) && $wp_query->is_main_query()) {
+            $wp_query->set('tax_query', array(
+                array (
+                    'taxonomy' => 'product_cat',
+                    'field' => 'slug',
+                    'terms' => $wp_query->query_vars['product_cat'],
+                    'include_children' => false
+                )
+            ) );
+        }
+    } 
+}
+    
+add_filter('pre_get_posts', 'exclude_product_cat_children', PHP_INT_MAX );
