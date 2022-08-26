fbf7e7b1434b56084896db1571307f6e29e41656
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-08-26 21:24:23 +0800

    Seasons

--- woocommerce/taxonomy-product-cat.php
+++ woocommerce/taxonomy-product-cat.php
@@ -110,14 +110,28 @@ get_header( 'shop' );
                 return;
               }
             
+              $paged                   = (get_query_var('paged')) ? absint(get_query_var('paged')) : 1; // if your custom loop is on a static front page then check for the query var 'page' instead of 'paged', see https://developer.wordpress.org/reference/classes/wp_query/#pagination-parameters
+              $ordering                = WC()->query->get_catalog_ordering_args();
+              $ordering['orderby']     = array_shift(explode(' ', $ordering['orderby']));
+              $ordering['orderby']     = stristr($ordering['orderby'], 'price') ? 'meta_value_num' : $ordering['orderby'];
+              $products_per_page       = apply_filters('loop_shop_per_page', wc_get_default_products_per_row() * wc_get_default_product_rows_per_page());
             
               $products_ids            = wc_get_products(array(
-                //'category'             => array('wam'),
                 'status'               => 'publish',
                 'limit'                => 1,
+                'page'                 => $paged,
+                'paginate'             => true,
                 'return'               => 'ids',
-                'include'              => array('511'),
+                'orderby'              => $ordering['orderby'],
+                'order'                => $ordering['order'],
               ));
+            
+              wc_set_loop_prop('current_page', $paged);
+              wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
+              wc_set_loop_prop('page_template', get_page_template_slug());
+              wc_set_loop_prop('per_page', $products_per_page);
+              wc_set_loop_prop('total', $products_ids->total);
+              wc_set_loop_prop('total_pages', $products_ids->max_num_pages);
             
               if($products_ids) {
                 do_action('woocommerce_before_shop_loop');
@@ -125,7 +139,7 @@ get_header( 'shop' );
                   foreach($products_ids->products as $featured_product) {
                     $post_object = get_post($featured_product);
                     setup_postdata($GLOBALS['post'] =& $post_object);
-                    wc_get_template_part('content', 'single-product');
+                    wc_get_template_part('content', 'product');
                   }
                   wp_reset_postdata();
                 woocommerce_product_loop_end();
@@ -134,7 +148,7 @@ get_header( 'shop' );
                 do_action('woocommerce_no_products_found');
               }
 
-            ?>
+              ?>
         </div>
 
     </div>
